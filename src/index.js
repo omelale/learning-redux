9b388feeee2898578e02b232574506521a91662c
Ajten <ajtencani@gmail.com>
2022-01-05 21:55:31 +0100

    added basic state management with redux

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {createStore} from "redux";
 
-//first thing we need is the store, which is the container for all the states i want to save
 
 //second thing we need is an action, which describes actions that can be performed on saved states in the store
 const increment = () => {
@@ -22,8 +21,23 @@ const decrement = () => {
 }
 
 //third thing is the reducer, which describe how the actions transform the state into next state
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMENT':
+            return state - 1
+    }
+}
+
+//first thing we need is the store, which is the container for all the states i want to save
+let store = createStore(counter);
+
+//to display the state
+store.subscribe(()=>console.log(store.getState()));
 
 //final step is dispatch, that executes the action (dispatch action to reducer)
+store.dispatch(increment());
 
 ReactDOM.render(
   <React.StrictMode>
