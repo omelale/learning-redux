b22cc531ae7d39a4753346ab19f09f5e2115fe0d
Ajten <ajtencani@gmail.com>
2022-01-05 21:04:50 +0100

    import ceratestore

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
 
 //first thing we need is the store, which is the container for all the states i want to save
 
