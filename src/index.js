203f55dbba0f2b370d9e58a357ac6155798f0c42
Ajten <ajtencani@gmail.com>
2022-01-05 21:14:59 +0100

    added the 2 actions, increment and decrement

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ import {createStore} from "redux";
 //first thing we need is the store, which is the container for all the states i want to save
 
 //second thing we need is an action, which describes actions that can be performed on saved states in the store
+const increment = () => {
+    return {
+    //    people here put the name of the action
+        type : 'INCREMENT'
+    }
+}
+
+const decrement = () => {
+    return {
+        type : 'DECREMENT'
+    }
+}
 
 //third thing is the reducer, which describe how the actions transform the state into next state
 
